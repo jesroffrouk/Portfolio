565f0fd4b52c0b52c38559aea0aa19b377aea2f9
jesroffrouk <jesroffrouk@gmail.com>
2025-11-09 20:44:04 +0530

    imported css file to main to fix errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from 'react-router'
-
+import './index.css'
+import './App.css'
 const App = React.lazy(()=> import('./App.jsx'))
 const Projects = React.lazy(()=> import('./pages/Projects.jsx'))
 const ProjectsList = React.lazy(()=> import('./pages/ProjectList.jsx'))
